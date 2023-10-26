e7f5d3c5ca5e943fedd327b80a68a6161bba70c4
Sagar ghare <sagar.ghare@acc.ltd>
2023-10-26 17:59:04 +0530

    thunk added

--- src/Redux/storage.js
+++ src/Redux/storage.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import userReducer from './Reducer/user.reducer';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-const storage = createStore(userReducer);
+const storage = createStore(userReducer, applyMiddleware(thunk));
 
 export default storage;
